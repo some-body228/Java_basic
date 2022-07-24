4452e1dcfd88fe4696cefe1a2b3bb18aeabdb39d
Remoute KA3AX <Some-body228@yandex.ru>
2022-07-24 11:28:03 +0300

    Quiz homework

--- HomeWork.java
+++ HomeWork.java
@@ -1,9 +1,37 @@
+import java.util.Base64;
+import java.util.Scanner;
 public class HomeWork {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter you full name");
-        String fullName = sc.nextLine();
-        String encodedFullName = Base64.getEncoder().encodeToString(fullName.getBytes());
-        System.out.println("Program result: " + encodedFullName);
+        String[] quiz = {"Правильный ответ 2",
+                "Правильный ответ 3",
+                "Правильный ответ 3",
+                "Правильный ответ 1",
+                "Правильный ответ 4"};
+        String[][] answerOptions = {{"Не правильный ответ","Правильный ответ","Не правильный ответ","Не правильный ответ"},
+                {"Не правильный ответ","Не правильный ответ","Правильный ответ","Не правильный ответ"},
+                {"Не правильный ответ","Не правильный ответ","Правильный ответ","Не правильный ответ"},
+                {"Правильный ответ","Не правильный ответ","Не правильный ответ","Не правильный ответ"},
+                {"Не правильный ответ","Не правильный ответ","Правильный ответ","Не правильный ответ"}};
+        Integer[] rightQuesion = {1, 2, 2, 0, 3};
+
+        Integer result = 0;
+        for(int i = 0; i < quiz.length; i++){
+            System.out.println(i+1 + ". " + quiz[i]);
+                for (String option: answerOptions[i]){
+                    System.out.println(i+1 + ". " + option);
+                }
+            while (true){
+                System.out.println("Ваш ответ:");
+                Integer answer = sc.nextInt();
+                if(answer > 0 && answer <= 4 ){
+                    if (rightQuesion[i].equals(answer - 1)){
+                        result++;
+                    }
+                    break;
+                } else System.out.println("ответ не прошел валидацию");
+            }
+        }
+        System.out.println("резуьтат " + result + " из 5");
     }
 }
