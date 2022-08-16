14c85e2bd10f31acab14078e795f8535f2ed6b58
Remoute KA3AX <Some-body228@yandex.ru>
2022-08-16 21:39:12 +0300

    Quiz homework

--- HomeWork.java
+++ HomeWork.java
@@ -1,5 +1,6 @@
 import java.util.Base64;
 import java.util.Scanner;
+
 public class HomeWork {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,19 +14,31 @@ public class HomeWork {
                 {"Не правильный ответ","Не правильный ответ","Правильный ответ","Не правильный ответ"},
                 {"Правильный ответ","Не правильный ответ","Не правильный ответ","Не правильный ответ"},
                 {"Не правильный ответ","Не правильный ответ","Правильный ответ","Не правильный ответ"}};
-        Integer[] rightQuesion = {1, 2, 2, 0, 3};
+        int[] rightQuesion = {1, 2, 2, 0, 3};
 
-        Integer result = 0;
+        int result = 0;
         for(int i = 0; i < quiz.length; i++){
             System.out.println(i+1 + ". " + quiz[i]);
-                for (String option: answerOptions[i]){
-                    System.out.println(i+1 + ". " + option);
-                }
+            for (int j = 0; j < answerOptions.length - 1; j++) {
+                System.out.println(j+1 + ". " + answerOptions[i][j]);
+            }
+//                for (String option: answerOptions[i]){
+//                    System.out.println(i+1 + ". " + option);
+//                }
             while (true){
                 System.out.println("Ваш ответ:");
-                Integer answer = sc.nextInt();
+                int answer = 0;
+                while (sc.hasNext()){
+                    if(sc.hasNextInt()){
+                        answer = sc.nextInt();
+                        break;
+                    } else {
+                        System.out.println("ответ не прошел валидацию");
+                        sc.next();
+                    }
+                }
                 if(answer > 0 && answer <= 4 ){
-                    if (rightQuesion[i].equals(answer - 1)){
+                    if (rightQuesion[i] == answer - 1){
                         result++;
                     }
                     break;
