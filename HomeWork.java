9f89eed9d6a56ff94b3ee49715ad5ca2df8c9f0b
Remoute KA3AX <Some-body228@yandex.ru>
2022-09-12 01:45:15 +0300

    Exceptions homework

--- HomeWork.java
+++ HomeWork.java
@@ -2,20 +2,43 @@ import java.util.*;
 
 public class HomeWork {
     public static void main(String[] args) {
-        // сортировка выбором
-        int[] list= new int[]{1, 3, 5, 5, 6, 1, 5, 1, 3};
-        for (int i = 0; i < list.length; i++) {
+        int[] list = new int[10000];
+
+        for(int i = 0; i < list.length; ++i) {
+            list[i] = (int)(Math.random() * 10.0);
+        }
+
+        long start1 = System.currentTimeMillis();
+
+        int j;
+        for(int i = 0; i < list.length; ++i) {
             int Nmin = i;
             int min = list[i];
-            for (int j = i+1; j < list.length; j++) {
-                if(min > list[j]){
+
+            for(j = i + 1; j < list.length; ++j) {
+                if (min > list[j]) {
                     min = list[j];
                     Nmin = j;
                 }
             }
+
             list[Nmin] = list[i];
             list[i] = min;
         }
+
+        long finish1 = System.currentTimeMillis();
         System.out.println(Arrays.toString(list));
+        System.out.println(finish1 - start1);
+        List<Integer> list2 = new ArrayList<>();
+
+        for(j = 0; j < 10000; ++j) {
+            list2.add((int)(Math.random() * 10.0));
+        }
+
+        long start2 = System.currentTimeMillis();
+        list2.sort(Comparator.comparingInt(o -> o));
+        long finish2 = System.currentTimeMillis();
+        System.out.println(list2);
+        System.out.println(finish2 - start2);
     }
 }
