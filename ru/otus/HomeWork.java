1c2f870881e839c2de12f74543c7a4c57358e71e
Remoute KA3AX <Some-body228@yandex.ru>
2022-10-06 21:02:27 +0300

    homework

--- ru/otus/HomeWork.java
+++ ru/otus/HomeWork.java
@@ -16,17 +16,8 @@ public class HomeWork {
         Информацию о пройденном тесте предлагается выводить в System.out, а об упавшем в System.err
      */
     public static void main(String[] args) {
-        diceImplTest();
-    }
-
-    public static void diceImplTest() {
-        DiceImpl diceImpl = new DiceImpl();
-        int result = diceImpl.roll();
-        System.out.println(result);
-        System.out.println(result <= 6);
-        assert result <= 6;
-        assert result >= 1;
 
     }
-    
+
+
 }
